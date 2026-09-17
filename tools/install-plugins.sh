a76fd7489d48148a4dded75c78679511f754cc0d
NaoyaNakamori <73022482+nakamori-naoya@users.noreply.github.com>
2026-09-17 22:38:33 +0900

    Register RDB design plugin for runtime installation

--- tools/install-plugins.sh
+++ tools/install-plugins.sh
@@ -31,6 +31,7 @@ MARKETPLACES='
 grill|grill-plugins|grill
 agent-work-policy|agent-work-policy-plugins|agent-work-policy
 write-doc|write-doc-plugins|write-doc
+rdb-design|rdb-design-plugins|rdb-design
 product-planning|product-planning-plugins|product-planning
 bdd-discovery-and-formulation|bdd-discovery-and-formulation-plugins|bdd-discovery-and-formulation
 domain-modeling|domain-modeling-plugins|domain-modeling
